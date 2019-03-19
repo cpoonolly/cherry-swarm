0b650776ce6dc51f33db05bd52f902d7a3245a90
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-19 16:14:18 +0700

    FEAT - Adding pretty orbit logic

--- cherry-logo.js
+++ cherry-logo.js
@@ -53,10 +53,40 @@ class CherryLogoSwarm {
         });
     }
 
-    renderSwarm(canvasContext) {
-        canvasContext.fillStyle = 'red';
-
+    orbitCircleAnimation(dt, circleCenterX, circleCenterY, radius) {
         this.particles.forEach((particle) => {
+            particle.x += dt * particle.dx / 1000;
+            particle.y += dt * particle.dy / 1000;
+
+            particle.x = Math.min(Math.max(0, particle.x), this.xMax);
+            particle.y = Math.min(Math.max(0, particle.y), this.yMax);
+
+            let vx = circleCenterX - particle.x;
+            let vy = circleCenterY - particle.y;
+            let vLength = Math.sqrt((vx*vx) + (vy*vy));
+
+            let vxNorm = vx / vLength;
+            let vyNorm = vy / vLength;
+
+            particle.dx += (vLength > radius ? 10 * vxNorm : 0.5 * vxNorm);
+            particle.dy += (vLength > radius ? 10 * vyNorm : 0.5 * vyNorm);
+
+            let vxPerpNorm = vy / vLength;
+            let vyPerpNorm = -1 * vx / vLength;
+
+            particle.dx += vxPerpNorm;
+            particle.dy += vyPerpNorm;
+
+            // console.log(`x=${particle.x}\ty=${particle.y}`);
+            // console.log(`dx=${particle.dx}\tdy=${particle.dy}`);
+        });
+    }
+
+    renderSwarm(currentTime, canvasContext) {
+        this.particles.forEach((particle, index) => {
+            let shade = 20 + (((currentTime / 1000) + (index / 100)) % 30);
+
+            canvasContext.fillStyle = `hsl(0,100%,${shade}%)`;
             canvasContext.fillRect(particle.x, particle.y, 5, 5);
         });
     }
@@ -79,7 +109,7 @@ class CherryLogo extends LitElement {
 
         this.width = 100;
         this.height = 100;
-        this.particleCount = 300;
+        this.particleCount = 1000;
         
         this.swarm = null;
         this.lastAnimationFrameTime = null;
@@ -94,7 +124,7 @@ class CherryLogo extends LitElement {
         this.swarm = new CherryLogoSwarm(this.width, this.height, this.particleCount);
         
         requestAnimationFrame((timestamp) => this.updateCherryLogo(timestamp));
-        setInterval(() => this.updateMoveToPoint(), 1000);
+        setInterval(() => this.updateMoveToPoint(), 5000);
     }
 
     updateMoveToPoint() {
@@ -103,7 +133,6 @@ class CherryLogo extends LitElement {
     }
     
     updateCherryLogo(currentTime) {
-        console.log('updateCherryLogo');
         if (!this.lastAnimationFrameTime) this.lastAnimationFrameTime = currentTime;
 
         const canvas = this.shadowRoot.getElementById(CHERRY_LOGO_CANVAS_ID);
@@ -112,11 +141,16 @@ class CherryLogo extends LitElement {
         const dt = currentTime - this.lastAnimationFrameTime;
 
         const canvasContext = canvas.getContext('2d');
-        canvasContext.fillStyle = 'black';
+
+        // this.shade = ((this.shade + (dt / 100)) % 30);
+        // canvasContext.fillStyle = `hsl(0,100%,${this.shade + 20}%)`;
+
+        canvasContext.fillStyle = 'white';
         canvasContext.fillRect(0, 0, this.width, this.height);
         
-        this.swarm.moveToPointAnimation(dt, this.moveToX, this.moveToY);
-        this.swarm.renderSwarm(canvasContext);
+        // this.swarm.moveToPointAnimation(dt, this.moveToX, this.moveToY);
+        this.swarm.orbitCircleAnimation(dt, this.moveToX, this.moveToY, 50);
+        this.swarm.renderSwarm(currentTime, canvasContext);
 
         this.lastAnimationFrameTime = currentTime;
 
